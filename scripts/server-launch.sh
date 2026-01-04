dd9e7d5da53572ada8b5b0fd27c67f5d6017d7f1
Timothy Hull <timothyhull@gmail.com>
2026-01-04 06:53:16 +0000

    Update config file target

--- scripts/server-launch.sh
+++ scripts/server-launch.sh
@@ -3,4 +3,4 @@
 # Start the MkDocs service
 ## .devcontainer/devcontainer.json runs this file as a post attach command
 ## --config-file specifies the Material for MkDocs config file
-mkdocs serve --config-file mkdocs.insiders.yml
+mkdocs serve --config-file mkdocs.yml
