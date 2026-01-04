eaec8c7df42f130a9358fddebd3e17021c7009b6
Timothy Hull <timothyhull@gmail.com>
2026-01-04 00:29:26 -0600

    Remove insiders install script

--- scripts/install-mkdocs-material-insiders.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install the Material for MkDocs Insiders package
-## .devcontainer/devcontainer.json runs this file as a post create command
-python -m pip install --upgrade git+https://${MKDOCS_TOKEN}@github.com/timothyhull/mkdocs-material-insiders.git
