7cbc518f8f9d963500bf17adbb0426ed000ee44e
Timothy Hull <timothyhull@gmail.com>
2026-01-04 00:30:09 -0600

    Revise launch command to remove insiders configuration file

--- scripts/server-launch.sh
+++ scripts/server-launch.sh
@@ -2,6 +2,5 @@
 
 # Start the MkDocs service
 ## .devcontainer/devcontainer.json runs this file as a post attach command
-## --config-file specifies the Material for MkDocs Insiders config file
-## --watch watches for changes in extra files for automatic page reloads
-mkdocs serve --config-file mkdocs.insiders.yml --watch mkdocs.yml # --watch ./includes
+## --config-file specifies the Material for MkDocs config file
+mkdocs serve --config-file mkdocs.insiders.yml
