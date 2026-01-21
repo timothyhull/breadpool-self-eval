db8ac9b6387afc7e8fc310ec1e4dedefe38f72c8
Timothy Hull <timothyhull@gmail.com>
2026-01-21 01:49:16 +0000

    Linting correction

--- scripts/server-launch.sh
+++ scripts/server-launch.sh
@@ -2,4 +2,4 @@
 
 # Start the MkDocs service
 ## .devcontainer/devcontainer.json runs this file as a post attach command
-mkdocs serve --livereload  # Workaround for GitHub issue (4055) with mkdocs
+mkdocs serve --livereload # Workaround for GitHub issue (4055) with mkdocs
